a5cec61728a9ed06e2e2f67e4335dd9c55dd213a
wonmoLee <leewonmo94@gmail.com>
2020-09-02 10:00:26 +0900

    이미지 업로드 with Tag 구현 완료

--- src/main/java/com/insta/sns/web/ImageController.java
+++ src/main/java/com/insta/sns/web/ImageController.java
@@ -3,15 +3,37 @@ package com.insta.sns.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.insta.sns.config.auth.LoginUserAnnotation;
 import com.insta.sns.config.auth.dto.LoginUser;
+import com.insta.sns.service.ImageService;
+import com.insta.sns.web.dto.ImageReqDto;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ImageController {
+	
+	private final ImageService imageService;
 	
 	@GetMapping({"", "/", "/image/feed"})
 	public String feed(@LoginUserAnnotation LoginUser loginUser) {
 		return "image/feed";
+	}
+	
+	@GetMapping("/image/uploadForm")
+	public String imageUploadForm() {
+		return "image/image-upload";
+	}
+
+	@PostMapping("/image")
+	public String imageUpload(@LoginUserAnnotation LoginUser loginUser, ImageReqDto imageReqDto) {
+
+		imageService.사진업로드(imageReqDto, loginUser.getId());
+
+		return "redirect:/";
 	}
 	
 }
